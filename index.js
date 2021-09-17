8fe955cf87efebb287d5d979c38a7a8fd763e153
Sam Dillenburg <39334877+hackbraten68@users.noreply.github.com>
2021-09-17 23:07:26 +0200

    remarkup

--- index.js
+++ index.js
@@ -7,61 +7,79 @@ const workBtn = document.getElementById("work-btn")
 const shortBreakBtn = document.getElementById("short-break-btn")
 const longBreakBtn = document.getElementById("long-break-btn")
 
-workBtn.addEventListener("click", workShift )
-shortBreakBtn.addEventListener("click", shortBreak )
-longBreakBtn.addEventListener("click", longBreak )
+let numOfMins = 0
+let typeOfCountdown = ""
 
-function workShift() {
+workBtn.addEventListener("click", () => { startTimer(25)} )
+shortBreakBtn.addEventListener("click", () => { startTimer(5)} )
+longBreakBtn.addEventListener("click", () => { startTimer(15)} )
 
-    let minute = 24
-    let sec = 59
+function startTimer(time) {
+    let allowedTime = time * 60
 
+    showTime(allowedTime)
     setInterval(function () {
-        timerDisplay.innerHTML = minute + " : " + sec
-        sec--
-        if ( sec == 0) {
-            minute--
-            sec = 59
-            if ( minute == 0) {
-                minute = 25
-            }
-        }
-    } ,1000)
+        allowedTime--
+        showTime(allowedTime)
+    }, 1000)
 }
 
-function shortBreak() {
+function showTime(allowedTime) {
+    timerDisplay.innerText = `${Math.floor(allowedTime / 60)} : ${allowedTime % 60}`
+
+}
+
+// function workShift() {
+
+//     let minute = 24
+//     let sec = 59
+
+//     setInterval(function () {
+//         timerDisplay.innerHTML = minute + " : " + sec
+//         sec--
+//         if ( sec == 0) {
+//             minute--
+//             sec = 59
+//             if ( minute == 0) {
+//                 minute = 25
+//             }
+//         }
+//     } ,1000)
+// }
+
+// function shortBreak() {
     
-    let minute = 4
-    let sec = 59 
+//     let minute = 4
+//     let sec = 59 
     
-    setInterval(function () {
-        timerDisplay.innerHTML = minute + " : " + sec
-        sec--
-        if (sec == 0) {
-            minute--
-            sec = 59
-            if (minute == 0) {
-                minute = 5
-            }
-        }
-    },1000)
-}
+//     setInterval(function () {
+//         timerDisplay.innerHTML = minute + " : " + sec
+//         sec--
+//         if (sec == 0) {
+//             minute--
+//             sec = 59
+//             if (minute == 0) {
+//                 minute = 5
+//             }
+//         }
+//     },1000)
+// }
 
-function longBreak() {
+// function longBreak() {
 
-    let minute = 14
-    let sec = 59
+//     let minute = 14
+//     let sec = 59
 
-    setInterval(function () {
-        timerDisplay.innerHTML = minute + " : " + sec
-        sec--
-        if (sec == 0) {
-            minute--
-            sec = 59
-            if ( minute == 0) {
-                minute = 14
-            }
-        }
+//     setInterval(function () {
+//         timerDisplay.innerHTML = minute + " : " + sec
+//         sec--
+//         if (sec == 0) {
+//             minute--
+//             sec = 59
+//             if ( minute == 0) {
+//                 minute = 14
+//             }
+//         }
 
-    },1000)
-}
+//     },1000)
+// }
